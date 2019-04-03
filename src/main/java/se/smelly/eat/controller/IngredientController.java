dae59ed9026337149b35b5637d1942c01f4aeb37
Erik Svensson <eosvensson@gmail.com>
2019-04-03 22:31:47 +0200

    Added more to IngredientController

--- src/main/java/se/smelly/eat/controller/IngredientController.java
+++ src/main/java/se/smelly/eat/controller/IngredientController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +14,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import se.smelly.eat.form.IngredientForm;
 import se.smelly.eat.models.Ingredient;
+import se.smelly.eat.models.Measurement;
 import se.smelly.eat.service.IngredientService;
 
 @RestController
@@ -35,5 +38,21 @@ public class IngredientController {
 		return ResponseEntity
 				.status(HttpStatus.CREATED)
 				.body(Mono.from(ingredientService.create(formMono)));		
+	}	
+	
+	@GetMapping("/ingredient/{id}")
+	public ResponseEntity<Mono<Ingredient>> getById(@PathVariable("id") String id){
+		Mono<Ingredient> resultMono = Mono.from(ingredientService.findById(id));		
+		return ResponseEntity.ok().body(resultMono);	
+	}
+	
+	@PutMapping("/ingredient/{id}")
+	public ResponseEntity<Mono<Ingredient>> update(@PathVariable("id") String id, @RequestBody Ingredient ingredient){
+		return ResponseEntity.ok(ingredientService.save(ingredient));
+	}
+	
+	@GetMapping("/ingredient/measurement")
+	public ResponseEntity<Flux<Measurement>> getMeasurements(){
+		return ResponseEntity.ok().body(Flux.fromArray(Measurement.values()));
 	}
 }
